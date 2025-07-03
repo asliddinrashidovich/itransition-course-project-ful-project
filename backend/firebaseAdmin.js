f7a67eb7415b2e10b19dd9625baaba62dd73727e
Asliddin <asliddinrashidovich7@gmail.com>
2025-07-03 20:09:35 +0500

    the firbase is changed

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
+
+// private_key dagi \\n larni real newline '\n' ga o‘zgartirish
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
